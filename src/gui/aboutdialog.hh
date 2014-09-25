20b9f6f8b1cb8e8a5eb776a68dc8a8bbe536a075
Michael Schaefer <michael.schaefer@uni-muenster.de>
2014-09-25 13:29:16 +0200

    aboutdialog: model part finished

--- src/gui/aboutdialog.hh
+++ src/gui/aboutdialog.hh
@@ -2,11 +2,12 @@
 #define __ABOUTDIALOG_HH__
 
 
-#include <QDebug>
+#include <QDesktopServices>
 #include <QDialog>
 #include <QFile>
 #include <QLocale>
 #include <QMessageBox>
+#include <QPalette>
 #include <QPushButton>
 #include <QVBoxLayout>
 #include <QUrl>
@@ -15,6 +16,9 @@
 
 
 class AboutDialog : public QDialog {
+
+    Q_OBJECT
+
 
 public:
 
@@ -25,29 +29,49 @@ public:
 
     AboutDialog(QWidget* parent=0, Role role=Model)
         : QDialog(parent)
-    {        
-        m_webView = new QWebView(parent);
-        m_webView->setHtml(getContent(role));
-        m_webView->show();
-        //m_webView->setStyleSheet("background: transparent");
-        m_webView->setAttribute(Qt::WA_TranslucentBackground);
+    {
+        QUrl url = getContentUrl(role);
+
+        m_webView = new QWebView(this);
+        if (url.isEmpty() == true) {
+            m_webView->setHtml(Text::About::dataNotFound());
+        } else {
+            m_webView->load(url);
+        }
+
+        m_webView->show();        
+        QPalette palette = m_webView->palette();
+        palette.setBrush(QPalette::Base, Qt::transparent);
+        m_webView->page()->setPalette(palette);
+        m_webView->setAttribute(Qt::WA_OpaquePaintEvent, false);
+        m_webView->page()->setLinkDelegationPolicy(QWebPage::DelegateExternalLinks);
+        QObject::connect(m_webView, SIGNAL(linkClicked(QUrl)),
+                         this, SLOT(openLink(QUrl)));
 
         m_close = new QPushButton(this);
         m_close->setText(Text::About::close());
         QObject::connect(m_close, SIGNAL(clicked()), this, SLOT(close()));
 
-        QVBoxLayout* box = new QVBoxLayout();
+        QVBoxLayout* box = new QVBoxLayout();        
         box->addWidget(m_webView);
         box->addWidget(m_close, 0, Qt::AlignRight);
         setLayout(box);
     }
 
 
+private slots:
+
+    void openLink(QUrl url) {
+        QDesktopServices::openUrl(url);
+    }
+
+
 private:
 
-    QString getContent(Role role, bool defaultLocale=false) {
-        QFile* file = 0;
+    QUrl getContentUrl(Role role, bool defaultLocale=false) {
+        QString filename;
         QString locale;
+        QUrl url;
 
         if (defaultLocale == false) {
             locale = QLocale::system().name().mid(0, 2);
@@ -56,22 +80,22 @@ private:
         }
 
         if (role == Model) {
-            file = new QFile(":/html/model_" + locale + ".html");
+            filename = ":/html/model_" + locale + ".html";
         } else {
-            file = new QFile(":/html/program_" + locale + ".html");
-        }
+            filename = ":/html/program_" + locale + ".html";
+        }        
 
-        if (file->open(QIODevice::ReadOnly) == true) {
-            return QString(file->readAll());
+        if (QFile(filename).open(QIODevice::ReadOnly) == true) {
+            return QUrl("qrc" + filename);
         } else {
             if (defaultLocale == true) {
-                return Text::About::dataNotFound();
+                return QUrl();
             } else {
                 QMessageBox::information(this, Text::Application::name(), Text::About::defaultLocaleText());
-                return getContent(role, true);
+                return getContentUrl(role, true);
             }
         }
-    }
+    }    
 
 
     QPushButton* m_close;
